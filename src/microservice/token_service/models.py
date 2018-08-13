0015d8393520c9d1abfcfe4f5db5afb4f3fe6d08
Kyle Ferriter <kylerferriter@gmail.com>
2018-08-13 18:20:51 -0400

    require user_name to be unique

--- src/microservice/token_service/models.py
+++ src/microservice/token_service/models.py
@@ -28,7 +28,7 @@ class EncryptedTextField(models.TextField):
 
 class User(models.Model):
     id = models.CharField(primary_key=True, max_length=256)
-    user_name = models.CharField(max_length=256)
+    user_name = models.CharField(unique=True, max_length=256)
     name = EncryptedTextField()
 
 '''
