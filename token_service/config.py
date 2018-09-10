a6c9222381b006b19af7d477049c439566946ebf
stdweird <stijn.deweirdt@ugent.be>
2018-09-10 15:16:44 +0200

    config: flake8 cleanup

--- token_service/config.py
+++ token_service/config.py
@@ -1,3 +1,4 @@
-admin_key=None
-db_key=None
-Config=None # pointer for app-wide config loaded from config.json
+admin_key = None
+db_key = None
+Config = None  # pointer for app-wide config loaded from config.json
+debug_sensitive = False
