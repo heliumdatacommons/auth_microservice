80fc41c451179b181a34a092c45095423344c30f
stdweird <stijn.deweirdt@ugent.be>
2018-09-10 15:17:59 +0200

    urls: flake8 cleanup

--- token_service/urls.py
+++ token_service/urls.py
@@ -1,4 +1,5 @@
-"""microservice URL Configuration
+"""
+token_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 try:
     from django.urls import url
 except ImportError:
@@ -38,5 +38,3 @@ urlpatterns = [
     url('^apikey/(?P<uid>[^/]+)/(?P<key_id>[^/]+)/?$', views.action_user_key),
     url('^apikey/verify/?$', views.verify_user_key)
 ]
-
-
