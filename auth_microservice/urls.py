07e2b4f045926cd95f01f2e5f71e6e1e658856cf
stdweird <stijn.deweirdt@ugent.be>
2018-09-17 11:00:08 +0200

    auth_microservice: urls: add missing import

--- auth_microservice/urls.py
+++ auth_microservice/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import token_service.urls
 from django.contrib import admin
 try:
     from django.urls import url, include
